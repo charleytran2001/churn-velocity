a28d007750e75c88aaca03d5f0e68195b80b207f
charleytran2001 <44280830+charleytran2001@users.noreply.github.com>
2023-02-15 10:33:42 -0700

    Production Build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-const queryClient = new QueryClient();
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
-// pages
+// components & pages
+import Nav from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
+    const { data: cards, refetch, isLoading, isFetching } = useQuery('cards', async () => {
+        try {
+            const { data } = await axios.get('https://churn-velocity-api.onrender.com/api/cards/');
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    if(isLoading || isFetching) {
+        return (
+            <h1>Cards Loading</h1>
+        );
+    }
+
     return (
-        <QueryClientProvider client={queryClient}>
-            <div className='App'>
-                <Home />
-            </div>
-        </QueryClientProvider>
+        <div className='App'>
+            <Nav cards={cards} />
+            <Home cards={cards} refetch={refetch} />
+        </div>
     );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,38 +1,20 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
-
 // components
-import Navbar from '../components/Navbar';
 import CardDetails from '../components/CardDetails';
 import CardForm from '../components/CardForm';
 
-const Home = () => {
-    const { data: cards, refetch } = useQuery('cards', async () => {
-        try {
-            const { data } = await axios.get('/api/cards/');
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
-    });
-
+const Home = ({cards, refetch}) => {
     return (
-        <div className='wrapper'>
-            <Navbar cards={cards} />
-
-            <div className='home'>
-                <div className='cards'>
-                    <h2>Your Cards</h2>
-                    {cards && cards.map(card => (
-                        <CardDetails key={card._id} card={card} refetch={refetch} />
-                    ))}
-                </div>
-
-                <div className='form'>
-                    <CardForm refetch={refetch} />
-                </div>
+        <div className='home'>
+            <div className='cards'>
+                <h2>Your Cards</h2>
+                {cards && cards.map(card => (
+                    <CardDetails key={card._id} card={card} refetch={refetch} />
+                ))}
             </div>
 
+            <div className='form'>
+                <CardForm refetch={refetch} />
+            </div>
         </div>
     );
 }
